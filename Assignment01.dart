9d28db23575763c2cdc5c031315b74570692dbcb
Asikuszzaman <114487026+AsikZaman11@users.noreply.github.com>
2023-08-04 00:27:10 +0600

    Add files via upload

---
+++ Assignment01.dart
@@ -0,0 +1,24 @@
+void main() {
+  const int a = 6;
+  const int b = 3;
+
+  //addition
+  var sum = a + b;
+  print("Addition: $a + $b = $sum");
+
+  //subtraction
+  var sub = a - b;
+  print("Subtraction: $a - $b = $sub");
+
+  //multiplication
+  var mul = a * b;
+  print("Multiplication: $b * $a =  $mul");
+
+  //division
+  var div = a / b;
+  print("Division: $b / $a =  $div");
+
+  //modulus
+  var mod = a % b;
+  print("Modulus: $b % $a =  $mod");
+}
